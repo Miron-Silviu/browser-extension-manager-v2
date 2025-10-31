42d7da80bc4b705e6126d4db1d1f21873e8a38a9
Silviu Miron <silviu_miron_90@outlook.com>
2025-10-31 23:05:48 +0000

    Feat: Create dark theme for mobile device .

--- docs/mobile-dark.js
+++ docs/mobile-dark.js
@@ -1,3 +1,9 @@
 'use strict';
 
-const body = document.getElementsByTagName('body');
+const toogleButton = document.querySelector('.toggle-button');
+const mainContainer = document.querySelector('.main-container');
+
+toogleButton.addEventListener('click', function () {
+  mainContainer.style.background =
+    'linear-gradient(180deg, #040918 0%, #091540 100%)';
+});
